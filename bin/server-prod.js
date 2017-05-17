dc122ce21eefaed413e9be43e27aa1200f7a9c9e
Sergey Zubov <serzbv@gmail.com>
2017-05-17 22:52:39 +0300

    #SL-100

--- bin/server-prod.js
+++
@@ -1,7 +0,0 @@
-const config = require('../config')
-const server = require('../server/main-prod')
-const debug = require('debug')('app:bin:server')
-const port = 1337;
-
-server.listen(port)
-debug(`Server is now running at http://localhost:${port}.`)
